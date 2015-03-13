76ccdecd538d845700f2fe98f20871718f71cbe4
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-03-13 23:18:59 +0100

    imports

--- src/android/KeyGuard.java
+++ src/android/KeyGuard.java
@@ -7,6 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.view.WindowManager;
+
 public class KeyGuard extends CordovaPlugin {
 
     public KeyGuard() {
